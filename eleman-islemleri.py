ac97ac1a91c9ed101ff1629fcd5127d2991ae2d4
karakayaahmet <65498435+karakayaahmet@users.noreply.github.com>
2022-10-01 13:40:21 +0300

    eleman işlemleri oluşturuldu.

---
+++ eleman-islemleri.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+a = np.array([1,2,3,4,5])
+b = pd.Series(a)
+
+print(b)
+
+print[b[0]]
+
+print(b[0:3])
+
+seri = pd.Series([1,2,3,4,5], index=["A","B","C","D","E"])
+
+print(seri.index)
+
+print(seri.keys)
+
+print(seri.values)
+
+print(seri.items())
+
+# eleman sorgulama
+
+"a" in seri
+
+print("a" in seri)
+
+print("A" in seri)
+
